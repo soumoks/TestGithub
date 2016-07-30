62c42bb2bd6ec83eb11def89d5b3e75096050402
Sourabh <soumoks@gmail.com>
2016-07-30 17:08:19 +0530

    added a check for the port_scanner script

--- port_scanner.sh
+++ port_scanner.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#check for package installation
+
+if [$(dpkg -s nmap)!=0] then;
+        echo "nmap is not installed, installing..."
+        sudo apt-get install nmap > /dev/null
+        echo "package installed"
+fi
+
+
 echo -n "Enter domain/IP address > "
 read address
 echo -n "Enter port number > "
