4b0f51734fef39d1d8809aa309546bc3972aee76
Sourabh <soumoks@gmail.com>
2016-08-02 15:34:36 +0530

    Cleaning up code on port_scanner.sh

--- port_scanner.sh
+++ port_scanner.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 #check for package installation
 
-#if [$(dpkg -s nmap)!=0] then;
-#        echo "nmap is not installed, installing..."
-#        sudo apt-get install nmap > /dev/null
-#        echo "package installed"
-#fi
-
 if (dpkg --get-selections | grep -q nmap); then
-#	echo "nmap is installed"
 #If true, do nothing
-:
+#using the no-op command 
+	:
 else
 	echo "nmap not present on machine"
 	echo "Installing nmap.."
@@ -22,6 +16,5 @@ read address
 echo -n "Enter port number > "
 read port
 echo "================="
-#echo ""
-#echo "testing git commit"
+
 nmap -p $port $address | grep -i -A 1 state
