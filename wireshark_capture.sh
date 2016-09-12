2195d297f9138bb764a78079543a52ddc2ab1915
sourabh <soumoks@gmail.com>
2016-09-12 10:40:46 +0530

    adding comments to wireshark script

--- wireshark_capture.sh
+++ wireshark_capture.sh
@@ -5,6 +5,9 @@ echo "entered key is $key"
 echo "Enter the complete DNS name including the user login"
 read dns
 echo "entered dns name is $dns"
+#Create a pipe
 mkfifo /tmp/remote
+#start wireshark reading from the same pipe
 wireshark -k -i /tmp/remote
+#Dump the packet capture from the instance onto the earlier created pipe
 ssh -i "$key" "$dns" "sudo tcpdump -s 0 -U -n -w - -i eth0 not port 22" > /tmp/remote
