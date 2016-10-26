5be7fffd668fe8b4142e6292f5bcbbb4af00843f
Sourabh <soumoks@gmail.com>
2016-10-26 15:57:18 +0530

    Beautified the script

--- gen_self_signed_cert.sh
+++ gen_self_signed_cert.sh
@@ -6,6 +6,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #####STEP 1#####
 #####Generate Private Key#####
+#beautify
+echo "                                          "
 echo "Enter private key name, uses name 'server' by default"
 read private_key
 
@@ -19,6 +21,11 @@ fi
 
 echo "Generating private key $private_key.key"
 openssl genrsa -des3 -out $private_key.key 1024
+
+#beautify
+echo "						"
+echo "=========================================="
+echo "                                          "
 
 #####STEP 2#####
 #####Generate CSR####
@@ -36,12 +43,21 @@ fi
 echo "Generating CSR $CSR.csr"
 openssl req -new -key $DIR/$private_key.key -out $CSR.csr
 
+#beautify
+echo "                                          "
+echo "=========================================="
+echo "                                          "
+
 #####STEP 3#####
 #####Remove passphrase from key#####
 cp $private_key.key $private_key.key.org
 echo "Enter the passphrase in order to remove the passphrase"
 openssl rsa -in $private_key.key.org -out $private_key.key
 
+#beautify
+echo "                                          "
+echo "=========================================="
+echo "                                          "
 
 #####STEP 4#####
 #####Generating a Self-signed certificate
@@ -59,4 +75,11 @@ fi
 echo "Generating Cert $cert.crt"
 openssl x509 -req -days 365 -in $DIR/$CSR.csr -signkey $private_key.key -out $cert.crt
 
-echo "Generated files are $private_key.key , $CSR.csr and $cert.crt"
+#beautify
+echo "                                          "
+echo "=========================================="
+echo "                                          "
+
+echo "Generated files are $private_key.key,$CSR.csr and $cert.crt"
+#beautify
+echo "                                          "
