fbf4d73eeddeff5f6d2f093204d8153a40987336
Sourabh <soumoks@gmail.com>
2016-10-26 15:46:45 +0530

    made multiple changes to gen_self_signed_cert

--- gen_self_signed_cert.sh
+++ gen_self_signed_cert.sh
@@ -3,29 +3,13 @@
 #####Store the current directory path########
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-#private_key =
-
-#####Declare funtion - Detect empty variable#######
-empty ()
-{
-	local var1=$1
-	echo $var1
-        if [ -z "var1" ]
-        then
-                echo "using default name 'server'"
-                var1="server"
-        else
-                echo "using name $var1"
-        fi
-}
 
 #####STEP 1#####
 #####Generate Private Key#####
 echo "Enter private key name, uses name 'server' by default"
 read private_key
-#empty $private_key
 
-if [-z "$private_key]
+if [ -z $private_key]
 then 
 	echo "using default name 'server'"
 	private_key=server
@@ -39,24 +23,40 @@ openssl genrsa -des3 -out $private_key.key 1024
 #####STEP 2#####
 #####Generate CSR####
 echo "Enter the name of the CSR, uses name 'server' by default"
-####add an option to use default name#####
 read CSR
-empty $CSR
+
+if [ -z $CSR]
+then
+        echo "using default name 'server'"
+        CSR=server
+else
+        echo "using name $CSR"
+fi
+
 echo "Generating CSR $CSR.csr"
 openssl req -new -key $DIR/$private_key.key -out $CSR.csr
 
 #####STEP 3#####
 #####Remove passphrase from key#####
 cp $private_key.key $private_key.key.org
+echo "Enter the passphrase in order to remove the passphrase"
 openssl rsa -in $private_key.key.org -out $private_key.key
 
 
 #####STEP 4#####
 #####Generating a Self-signed certificate
 echo "Enter the name of the cert, uses name 'server' by default"
-#####Add an option to use default name#####
 read cert
-empty $cert
-echo "Generating cert $cert.crt"
+
+if [ -z $cert]
+then
+        echo "using default name 'server'"
+        cert=server
+else
+        echo "using name $cert"
+fi
+
+echo "Generating Cert $cert.crt"
 openssl x509 -req -days 365 -in $DIR/$CSR.csr -signkey $private_key.key -out $cert.crt
 
+echo "Generated files are $private_key.key , $CSR.csr and $cert.crt"
