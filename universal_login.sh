2c033e882c1bcce88939960a66437faf573eb867
Sourabh <soumoks@gmail.com>
2016-11-24 12:15:20 +0530

    still intitial commits, just adding some default values

--- universal_login.sh
+++ universal_login.sh
@@ -11,5 +11,15 @@ username=ec2-user
 #default directory for certificates
 #current directory
 cert_dir=$DIR
-echo "Current directory $cert_dir"
+#echo "Current directory is $cert_dir"
 
+#default cert
+cert=/Downloads/Mumbai.pem
+
+#default ssh command
+#echo "This is default ssh command ssh -i key_file ec2-user@IPadd/domain_name"
+
+#certs present in the default directory - current directory
+echo "These are the certs present in the default directory"
+find . -type f -name \*.pem >certs.txt
+cat certs.txt
