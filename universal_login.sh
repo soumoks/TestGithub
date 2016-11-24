8a22078102d6545a5ac05aa9d471b31c85eab9db
sourabh <soumoks@gmail.com>
2016-11-24 11:46:34 +0530

    initial commit

--- universal_login.sh
+++ universal_login.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+#####Store the current directory path########
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+##Defaults
+
+#Default username
+username=ec2-user
+
+#default directory for certificates
+#current directory
+cert_dir=$DIR
+echo "Current directory $cert_dir"
+
